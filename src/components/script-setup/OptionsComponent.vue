72eba8dc3a8ba89096673d784030c10365ab5bb1
chichoon <chichoon.choi@gmail.com>
2023-11-09 00:53:11 +0900

    feat: :sparkles: Options API 사용례 추가

---
+++ src/components/script-setup/OptionsComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>{{ n }}</h1>
+  <div class="message-wrapper">
+    {{ message }}
+  </div>
+  <input type="text" v-model="message" />
+</template>
+
+<script lang="ts">
+let intervalID: number;
+
+export default {
+  data: function () {
+    return {
+      n: 0,
+      message: '',
+    };
+  },
+  methods: {
+    add: function () {
+      this.n += 1;
+    },
+  },
+  mounted: function () {
+    intervalID = setInterval(() => {
+      this.add();
+    }, 1000);
+  },
+  unmounted: function () {
+    clearInterval(intervalID);
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.message-wrapper {
+  height: 20px;
+}
+</style>
--- src/views/SetupView.vue
+++ src/views/SetupView.vue
@@ -1,9 +1,11 @@
 <template>
   <SetupComponent />
   <ScriptSetupComponent />
+  <OptionsComponent />
 </template>
 
 <script setup lang="ts">
+import OptionsComponent from '@/components/script-setup/OptionsComponent.vue';
 import ScriptSetupComponent from '@/components/script-setup/ScriptSetupComponent.vue';
 import SetupComponent from '@/components/script-setup/SetupComponent.vue';
 </script>
